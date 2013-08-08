09ff489144445f9b4bf9ba7f8d37166667e33005
linlingyu <linlingyu@baidu.dom>
2013-08-08 19:30:24 +0800

    build

---
+++ bin/main.js
@@ -0,0 +1,31 @@
+void function(){
+    var httpServer = require('httpServer').create(),
+        httpProxy = require('httpProxy').create(httpServer);
+        socketServer = require('socketServer').create(),
+        socketProxy = require('socketProxy').create(socketServer),
+        codeController = require('CodeController').create();
+        
+    httpProxy.initialize(codeController);
+    socketProxy.initialize(codeController);
+    httpProxy.start();
+    socketProxy.start(httpProxy.getServer());
+    
+    var timer;
+    socketProxy.on('send:arrival', function(args){
+        var mapList = args.mapList,
+            codeList = args.codeList;
+        
+        clearTimeout(timer);
+        timer = setTimeout(function(){
+            var array = codeList.list();
+            array.forEach(function(item){
+                if(item.getCodeId() === 'codeId_0'){
+                    var line = item.getFormattedCode();
+                    debugger;
+//                    console.log(item._fileName);
+                }
+            });
+            
+        }, 1000);
+    });
+}();
