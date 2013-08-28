fe090d727b0c889676f2ba911c11a5da123db445
linlingyu <linlingyu@baidu.dom>
2013-08-28 14:25:23 +0800

    加入请求超时设置

--- bin/web.js
+++ bin/web.js
@@ -2,12 +2,17 @@
 var args = phantom.args,
     page = require('webpage').create(),
     url = args[0],
-    taskId = args[1];
+    taskId = args[1],
+    timer;
 console.log('phantomjs start: ' + url);
 page.clipRect = {top: 0, left: 0, width: 1280, height: 800};
 
 page.onCallback = function(data){
-    data.cmd === 'exit' && phantom.exit();
+    if(data.cmd === 'exit'){
+        clearTimeout(timer);
+        console.log('request tracker complete');
+        phantom.exit();
+    }
 }
 page.onLoadFinished = function(){
     page.evaluate(function(taskId){//该页面的唯一id，会通过socket pageload事件发送到nodejs里
@@ -17,7 +22,10 @@ page.onLoadFinished = function(){
             window.callPhantom({cmd: 'exit'});
         });
     }, taskId);
-    
+    var timer = setTimeout(function(){//超时20秒退出
+        console.log('request tracker failure');
+        phantom.exit();
+    }, 20000);
 }
 page.open(url, function (status) {
     //Page is loaded!
