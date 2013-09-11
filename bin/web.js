e7ddf2456a1019055c88705520e6d3288d218f95
linlingyu <linlingyu@baidu.dom>
2013-09-11 19:14:31 +0800

    奇怪的bug

--- bin/web.js
+++ bin/web.js
@@ -22,6 +22,7 @@ page.onCallback = function(data){
     phantom.exit();
 }
 page.onLoadFinished = function(){
+    //这里需要有一行回车，奇怪的bug
     page.evaluate(function(taskId){//该页面的唯一id，会通过socket pageload事件发送到nodejs里
         window._taskId_ = taskId;
         window._socketFinish_ = function(){
